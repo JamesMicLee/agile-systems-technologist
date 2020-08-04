0b0cd422fba551034d419f5acd054d8ad5f9355a
James Lee <jamesmiclee@gmail.com>
2020-08-04 10:00:42 +0000

    Adds test_py.bash and updates .gitignore.

---
+++ python/3/aws/test_py.bash
@@ -0,0 +1,27 @@
+#!/bin/bash 
+#Amazon Linux 2.
+
+# Install python3
+which python3 >/dev/null
+if [ $? -ne 0 ]
+then
+  yum install -y python3
+fi
+
+# Remove the old virtual environment.
+if [ -d ./.py_test_venv ]
+then
+  rm -fr ./.py_test_venv
+fi
+
+# Make a new virtual environment.
+python3 -m venv .py_test_venv
+. .py_test_venv/bin/activate
+
+# Install awscommon which includes botocore and boto3.
+pip -q install flake8 
+
+# List out what is installed.
+# pip list --format=columns
+
+flake8 *.py
