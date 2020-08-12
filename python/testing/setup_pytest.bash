6e1e1f61a57d83c58b347e41fa8f51e56eed9c94
James Michael Lee <jamesmiclee@gmail.com>
2020-08-12 07:31:17 +0100

    Adds setup_pytest.bash and setup_flake8.bash

---
+++ python/testing/setup_pytest.bash
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+
+#Amazon Linux 2
+
+# If a new virtual env was requested, then remove any old one.
+if [ ${1:-'no'} == "new" ]
+then
+  if [ -d .venv ]
+  then
+    rm -rfv .venv
+  fi
+fi
+
+# If there is no virtual env, then create one.
+if [ ! -d .venv ]
+then
+  /usr/bin/env python3 -m venv .venv
+fi
+
+# Activate the virtual env.
+. .venv/bin/activate
+
+# pip install the tools
+pip install wheel
+pip install pytest-benchmark
+pip list --format=columns
